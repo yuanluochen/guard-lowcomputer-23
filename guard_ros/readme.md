303106a0999b37937eab99bebe2160d3a059762d
yuanluochen <yuanluochen@foxmail.com>
2023-01-06 16:12:26 +0800

    该版本,经过验证可以接收发布者消息并发布数据,上一个版本的问题在于未创建订阅者指针

---
+++ guard_ros/readme.md
@@ -0,0 +1,5 @@
+# 哨兵工程--上位机部分
+
+## 节点
+
+serial_cpp_node节点，订阅serial话题的内容，将serial话题的通过串口发送
--- guard_ros/src/serial/src/main.cpp
+++ guard_ros/src/serial/src/main.cpp
@@ -12,7 +12,7 @@ public:
     {
         this->topicName = topicName;
         //创建订阅者
-        this->create_subscription<std_msgs::msg::String>(this->topicName, 10, std::bind(&serialTx::messageHandle, this, std::placeholders::_1));
+        serialSubscription_ = this->create_subscription<std_msgs::msg::String>(this->topicName, 10, std::bind(&serialTx::messageHandle, this, std::placeholders::_1));
         //串口初始化
         serial.init();
         RCLCPP_INFO(this->get_logger(), "串口节点创建完毕，节点订阅话题为 >> %s <<", this->topicName.c_str());
@@ -21,11 +21,15 @@ public:
 private:
     void messageHandle(std_msgs::msg::String topicMessage)
     {
+        //发布
+        RCLCPP_INFO(this->get_logger(), "读取到话题%s·的数据， 数据为 >> %s <<", this->topicName.c_str(), topicMessage.data.c_str());
         //读取获取节点数据，节点数据发送到串口
         serial.sendMessage(topicMessage.data, strlen(topicMessage.data.c_str()));
         //打印发送的数据
         RCLCPP_INFO(this->get_logger(), "发送的数据为 >> %s <<\n", serial.getTxMessage().c_str());
     }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr serialSubscription_;
+    
 };
 
 int main(int argc, char** argv)
