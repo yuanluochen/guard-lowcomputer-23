994ecf4f4d7e3aed5602a09250b593cbf0b61df3
yuanluochen <yuanluochen@foxmail.com>
2023-01-06 12:46:22 +0800

    修改了一些内容

--- guard_ros/src/serial/src/main.cpp
+++ guard_ros/src/serial/src/main.cpp
@@ -6,13 +6,16 @@ class serialTx : public rclcpp::Node
 {
 private:
     SerialSpace::Serial serial;
+    std::string topicName;
 public:
-    serialTx() : Node("serial_cpp_node"), serial(SerialSpace::SERIAL_BAUDRATE_115200, SerialSpace::NO_FLOW_CTRL, SerialSpace::DATA_8BIT, SerialSpace::NO_PARITY, SerialSpace::NO_STOP_BIT)
+    serialTx(std::string topicName) : Node("serial_cpp_node"), serial(SerialSpace::SERIAL_BAUDRATE_115200, SerialSpace::NO_FLOW_CTRL, SerialSpace::DATA_8BIT, SerialSpace::NO_PARITY, SerialSpace::NO_STOP_BIT)
     {
+        this->topicName = topicName;
         //创建订阅者
-        this->create_subscription<std_msgs::msg::String>("serial", 10, std::bind(&serialTx::messageHandle, this, std::placeholders::_1));
+        this->create_subscription<std_msgs::msg::String>(this->topicName, 10, std::bind(&serialTx::messageHandle, this, std::placeholders::_1));
         //串口初始化
         serial.init();
+        RCLCPP_INFO(this->get_logger(), "串口节点创建完毕，节点订阅话题为 >> %s <<", this->topicName.c_str());
     }
 
 private:
@@ -29,7 +32,7 @@ int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
 
-    rclcpp::spin(std::make_shared<serialTx>());
+    rclcpp::spin(std::make_shared<serialTx>((char*)"serial"));
     rclcpp::shutdown();
     return 0;
 }
