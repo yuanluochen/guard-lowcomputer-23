c9926c582c03acd32ee1c32c9d21a2dc65689d72
yuanluochen <yuanluochen@foxmail.com>
2023-01-30 14:26:04 +0800

    更新一下哨兵的下位机

---
+++ Lowcomputer/application/servo_task.c
@@ -0,0 +1,45 @@
+/**
+  ****************************(C) COPYRIGHT 2019 DJI****************************
+  * @file       servo_task.c/h
+  * @brief      
+  * @note       
+  * @history
+  *  Version    Date            Author          Modification
+  *  V1.0.0     Oct-21-2019     RM              1. done
+  *
+  @verbatim
+  ==============================================================================
+
+  ==============================================================================
+  @endverbatim
+  ****************************(C) COPYRIGHT 2019 DJI****************************
+  */
+
+#include "servo_task.h"
+#include "main.h"
+#include "cmsis_os.h"
+#include "bsp_servo_pwm.h"
+#include "remote_control.h"
+#include "bsp_laser.h"
+#include "bsp_fric.h"
+#include "arm_math.h"
+#include "user_lib.h"
+#include "referee.h"
+#include "CAN_receive.h"
+#include "gimbal_behaviour.h"
+#include "detect_task.h"
+#include "pid.h"
+
+
+void servo_task()
+{
+
+    while(1)
+    {
+			
+
+    }
+		vTaskDelay(1);
+}
+
+
---
+++ Lowcomputer/application/servo_task.h
@@ -0,0 +1,6 @@
+#ifndef SERVO_TASK_H
+#define SERVO_TASK_H
+#include "struct_typedef.h"
+
+extern void servo_task();
+#endif
